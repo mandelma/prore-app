f800a4d3fc83e992c1dd6e528e045ae1321b4531
Marko <mandlimarko@gmail.com>
2026-05-06 22:43:11 +0300

    Fix depandencies

--- dist/assets/loadGoogleMap-DZ-zVA3K.js
+++
@@ -1,1 +0,0 @@
-import{L as o}from"./index-93e-Jyro.js";let e;function l(){return e||(e=new o({apiKey:"AIzaSyDt2YXE5tk0J72JgqnH3DTD7MeoqbbWBmU",version:"weekly",libraries:["places","geometry"]}).load()),e}export{l};
