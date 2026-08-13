3ee725b678a077f652a15436d5834e307a607cc0
Marko <mandlimarko@gmail.com>
2026-08-13 15:40:06 +0300

    ---

--- src/App.vue
+++ src/App.vue
@@ -1500,6 +1500,22 @@ const enablePushNotifications = async () => {
   }
 };
 
+const urlBase64ToUint8Array = base64String => {
+  const padding =
+    "=".repeat((4 - (base64String.length % 4)) % 4);
+
+  const base64 =
+    (base64String + padding)
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+
+  return Uint8Array.from(
+    [...rawData].map(char => char.charCodeAt(0))
+  );
+};
+
 const enableNotificationsFromModal = async () => {
   await enablePushNotifications();
   showNotificationModal.value = false;
