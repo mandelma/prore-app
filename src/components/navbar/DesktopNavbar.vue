123ae7b7d261cd9437cb219b3239c3f63522b510
Marko <mandlimarko@gmail.com>
2026-08-24 23:16:48 +0300

    Removed desktop nav install pwa button

--- src/components/navbar/DesktopNavbar.vue
+++ src/components/navbar/DesktopNavbar.vue
@@ -10,7 +10,7 @@
               @click="router.push('/')"
           />
         </MDBNavbarBrand>
-        <MDBNavbarNav v-if="!isAuthenticated && showInstallOption" right class="ms-auto d-flex flex-row align-items-center gap-2">
+        <!-- <MDBNavbarNav v-if="!isAuthenticated && showInstallOption" right class="ms-auto d-flex flex-row align-items-center gap-2">
           <MDBNavbarItem>
             <button
                 type="button"
@@ -21,7 +21,7 @@
                 <span>{{ t('pwa.install_app') }}</span>
             </button>
           </MDBNavbarItem>
-        </MDBNavbarNav>
+        </MDBNavbarNav> -->
         <MDBNavbarNav right class="ms-auto d-flex flex-row align-items-center gap-2">
 
           <!-- Client incomed offers -->
